d1193265264d8cd36bf3484e7139b93ac708535b
Lucas-Cox-CART <lucas.cox@student.cart.org>
2023-03-21 15:50:40 -0700

    save-quick
    save

---
+++ scripts/settings.js
@@ -0,0 +1,12 @@
+function gotoSettings() {
+    header.style.display = "none";
+    main.style.display = "none";
+    footer.style.display = "none";
+    settingsPage.style.display = "flex";
+}
+
+
+function startGame() {
+    settingsPage.style.display = "none";
+    mainGame.style.display = "flex";
+}
